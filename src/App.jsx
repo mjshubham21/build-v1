3db97cecd47408d4fe448473453d7e92b36b96fb
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-03-09 00:03:55 +0530

    day2 file updated nothing to improve no design created till now

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
+import Home from "./components/Home/Home";
 
 const App = () => {
   return (
     <>
-      <h1>hello this is a basic structure</h1>
-      <h2>we will add more details in next coming days</h2>
+      <Home />
     </>
   );
 };
